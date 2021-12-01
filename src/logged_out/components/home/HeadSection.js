e127832798caf83e61f81d386f02bc90a101994f
“Gustavo <gustavo.granato@hotmail.com>
2021-12-01 10:24:29 -0300

    change image

--- src/logged_out/components/home/HeadSection.js
+++ src/logged_out/components/home/HeadSection.js
@@ -64,7 +64,7 @@ const styles = (theme) => ({
     paddingBottom: theme.spacing(2),
   },
   image: {
-    maxWidth: "100%",
+    maxWidth: 400,
     verticalAlign: "middle",
     borderRadius: theme.shape.borderRadius,
   },
@@ -90,16 +90,14 @@ const styles = (theme) => ({
     paddingTop: theme.spacing(4),
   },
   flex: {
-    display: 'flex'
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
 function HeadSection(props) {
   const { classes, theme, width } = props;
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  // const id = '_form1_'
-  // axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-  // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
   useEffect(() => {
     const script = document.createElement('script');
@@ -110,49 +108,7 @@ function HeadSection(props) {
     document.body.appendChild(script);
   }, [])
 
-  const onSubmit = async (data) => {
-    const params = {
-      u: "1",
-      f: "1",
-      s: "",
-      c: "0",
-      m: "0",
-      act: "sub",
-      v: "2",
-      or: "b5b82f87de3111a61d0d3ea55d620e82",
-      email: data.email
-    }
-    const id = '_form_1_'
-    // const url = `https://gustavogranato.activehosted.com/proc.php?u=${id}&f=${id}&s=&c=0&m=0&act=sub&v=2&email=${data.email}&jsonp=true`;
-
-    const url = 'https://gustavogranato.activehosted.com/proc.php'
-    // fetch(url, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // });
-    try {
-      const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        // redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(params) // body data type must match "Content-Type" header
-      }).then(res => console.log(res, 'res')).catch(err => console.error(err, 'erro no catch'))
-      console.log(response, 'Response')
-      
-    } catch (error) {
-      console.log(error, 'errro trycath')
-    }
-  }
-  console.log(watch("email"));
+  
   return (
     <Fragment>
       <div className={classNames("lg-p-top", classes.wrapper)}>
@@ -172,10 +128,10 @@ function HeadSection(props) {
                       justifyContent="space-between"
                       height="100%"
                     >
-                      <Hidden smUp>
+                      <Hidden smUp >
                           <Grid item md={6} className={classes.flex}>
                             <img
-                              src={`${process.env.PUBLIC_URL}/images/logged_out/advento2.svg`}
+                              src={`${process.env.PUBLIC_URL}/images/logged_out/advento.png`}
                               className={classes.image}
                               style={{marginBottom: 20}}
                               alt="Mirella Dellazzari | E-book advento"
@@ -197,11 +153,20 @@ function HeadSection(props) {
                             color="textSecondary"
                             className={classes.description}
                           >
-                            Trouxemos 4 poemas e algumas poemas de exercícios para ser usados no tempo do advento.
-                            É possível trabalhar gramática, leitura em voz alta, interpretação de texto.
-                            São leituras simples para todas as idades, incluindo adultos.
-                            Deus deu aos pais a jurisdição sobre a educação dos filhos. 
-                            Nesse tempo propício do advento, como preparação para a chegada do Menino Deus, o estudo em família pode ser muito frutuoso!
+                            Olá meus amigos e meus inimigos!
+
+                            Para melhor aproveitar esse tempo troouxemos 4 poemas e algumas dicas de atividades  para ser usados com os pequenos no tempo do Advento.
+
+                            
+                          </Typography>
+                          <br />
+                          <Typography
+                            variant={isWidthUp("lg", width) ? "h2" : "body1"}
+                            color="textSecondary"
+                            className={classes.description}
+                          >
+                            Lembremos que Deus  deu aos pais a jurisdição sobre a educação dos filhos. 
+                            Nesse tempo propício do Advento, como preparação para a chegada do Menino Deus, o estudo em família pode ser muito frutuoso!
                             Aproveite bem o momento.
                           </Typography>
                         </Box>
@@ -213,7 +178,7 @@ function HeadSection(props) {
                   <Hidden smDown>
                     <Grid item md={6} className={classes.flex}>
                       <img
-                        src={`${process.env.PUBLIC_URL}/images/logged_out/advento2.svg`}
+                        src={`${process.env.PUBLIC_URL}/images/logged_out/advento.png`}
                         className={classes.image}
                         alt="Mirella Dellazzari | E-book advento"
                       />
